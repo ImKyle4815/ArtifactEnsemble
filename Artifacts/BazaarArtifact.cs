06309b6926741b7020f276a912ba635ae6b03709
oorzkws <65210810+oorzkws@users.noreply.github.com>
2022-03-22 14:40:40 +0000

    Fixes for Bazaar
    - Move initialization slightly later so we don't end up working with GC'd objects if the bazaar is immediately exited
    - Fix NRE when Artifact of Sacrifice is enabled (or if spawning the other items fails for some reason)
    
    I don't know if the NetworkServer check is necessary, this is my first work on R2API. I would recommend taking the suggestions instead of the code verbatim if you have more expertise there.

--- Artifacts/BazaarArtifact.cs
+++ Artifacts/BazaarArtifact.cs
@@ -1,4 +1,3 @@
-﻿using System;
 using RoR2;
 using UnityEngine;
 using UnityEngine.Networking;
@@ -11,42 +10,61 @@ namespace ArtifactEnsemble.Artifacts
         public BazaarArtifact()
         {
             Init("Bazaar", "Adds a cleansing pool, scrapper, and a void chest to the space bazaar.", Properties.Resources.bazaar_on, Properties.Resources.bazaar_off);
-            On.RoR2.BazaarController.Awake += SpawnBazaarExtras;
+            On.RoR2.BazaarController.SetUpSeerStations += SpawnBazaarExtras;
         }
 
-        private void SpawnBazaarExtras(On.RoR2.BazaarController.orig_Awake orig, BazaarController self)
+        private static void TrySpawn(string path, Vector3 pos, Vector3 ang)
+        {
+            // Director stuff
+            var directorPlacementRule = new DirectorPlacementRule
+            {
+                placementMode = DirectorPlacementRule.PlacementMode.Direct
+            };
+            
+            var spawnCard = Resources.Load<InteractableSpawnCard>(path);
+            if (!spawnCard)
+            {
+                ArtifactEnsemble.Logger.LogWarning($"Spawner failed to load spawn card with path \"{path}\"");
+                return;
+            }
+            
+            spawnCard.skipSpawnWhenSacrificeArtifactEnabled = false;// Override since we want this to show here
+            var spawnRequest = new DirectorSpawnRequest(spawnCard, directorPlacementRule, Run.instance.runRNG);
+            var spawnResult = spawnCard.DoSpawn(pos, Quaternion.identity, spawnRequest);
+            var spawnedInstance = spawnResult.spawnedInstance;
+            
+            if (!spawnResult.success||ReferenceEquals(spawnedInstance, null))
+            {
+                ArtifactEnsemble.Logger.LogWarning($"Spawning object with path \"{path}\" failed");
+                return;
+            }
+            
+            spawnedInstance.transform.eulerAngles = ang;
+            
+            if(NetworkServer.active)
+                NetworkServer.Spawn(spawnedInstance);
+        }
+
+        private void SpawnBazaarExtras(On.RoR2.BazaarController.orig_SetUpSeerStations orig, BazaarController self)
         {
             orig(self);
-            if (this.Enabled())
-            {
-                ArtifactEnsemble.Logger.LogInfo("Spawning bazaar extras.");
-                // Director stuff
-                DirectorPlacementRule directorPlacementRule = new DirectorPlacementRule();
-                directorPlacementRule.placementMode = DirectorPlacementRule.PlacementMode.Direct;
-                // Actual spawning
-                // Cleansing Pool
-                if (ArtifactEnsembleConfig.SpawnBazaarCleansingPool.Value)
-                {
-                    SpawnCard spawnCardCleansingPool = Resources.Load<SpawnCard>("SpawnCards/InteractableSpawnCard/iscShrineCleanse");
-                    GameObject spawnedInstanceCleansingPool = spawnCardCleansingPool.DoSpawn(new Vector3(-65.7f, -23.5f, -18.9f), Quaternion.identity, new DirectorSpawnRequest(spawnCardCleansingPool, directorPlacementRule, Run.instance.runRNG)).spawnedInstance;
-                    NetworkServer.Spawn(spawnedInstanceCleansingPool);
-                }
-                // Scrapper
-                if (ArtifactEnsembleConfig.SpawnBazaarScrapper.Value)
-                {
-                    SpawnCard spawnCardScrapper = Resources.Load<SpawnCard>("SpawnCards/InteractableSpawnCard/iscScrapper");
-                    GameObject spawnedInstanceScrapper = spawnCardScrapper.DoSpawn(new Vector3(-82.1f, -23.7f, -5.2f), Quaternion.identity, new DirectorSpawnRequest(spawnCardScrapper, directorPlacementRule, Run.instance.runRNG)).spawnedInstance;
-                    spawnedInstanceScrapper.transform.eulerAngles = new Vector3(0f, 72.6f, 0f);
-                    NetworkServer.Spawn(spawnedInstanceScrapper);
-                }
-                // Void Chest
-                if (ArtifactEnsembleConfig.SpawnBazaarVoidChest.Value)
-                {
-                    SpawnCard spawnCardVoidChest = Resources.Load<SpawnCard>("SpawnCards/InteractableSpawnCard/iscVoidChest");
-                    GameObject spawnedInstanceVoidCradle = spawnCardVoidChest.DoSpawn(new Vector3(-69.7f, -23.5f, -12.9f), Quaternion.identity, new DirectorSpawnRequest(spawnCardVoidChest, directorPlacementRule, Run.instance.runRNG)).spawnedInstance;
-                    NetworkServer.Spawn(spawnedInstanceVoidCradle);
-                }
-            }
+            
+            if (!this.Enabled()) return;
+            
+            ArtifactEnsemble.Logger.LogInfo("Spawning bazaar extras.");
+            
+            // Cleansing Pool
+            if(ArtifactEnsembleConfig.SpawnBazaarCleansingPool.Value)
+                TrySpawn("SpawnCards/InteractableSpawnCard/iscShrineCleanse", new Vector3(-65.7f, -23.5f, -18.9f), Vector3.zero);
+            
+            // Scrapper
+            if (ArtifactEnsembleConfig.SpawnBazaarScrapper.Value)
+                TrySpawn("SpawnCards/InteractableSpawnCard/iscScrapper", new Vector3(-82.1f, -23.7f, -5.2f),
+                    new Vector3(0f, 72.6f, 0f));
+            
+            // Void Chest
+            if (ArtifactEnsembleConfig.SpawnBazaarVoidChest.Value)
+                TrySpawn("SpawnCards/InteractableSpawnCard/iscVoidChest",new Vector3(-69.7f, -23.5f, -12.9f),Vector3.zero);
         }
     }
 }
