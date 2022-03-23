df58865591b9cf367b04a38ca2b531da991414de
oorzkws <65210810+oorzkws@users.noreply.github.com>
2022-03-22 20:20:21 -0600

    Bazaar fixes
    - Move initialization slightly later so we don't end up working with GC'd objects if the bazaar is immediately exited
    - Fix NRE when Artifact of Sacrifice is enabled (or if spawning the other items fails for some reason)

--- Artifacts/BazaarArtifact.cs
+++ Artifacts/BazaarArtifact.cs
@@ -1,7 +1,6 @@
-﻿using System;
-using RoR2;
+﻿using RoR2;
 using UnityEngine;
-using UnityEngine.Networking;
+using static ArtifactEnsemble.ArtifactEnsemble;
 
 namespace ArtifactEnsemble.Artifacts
 {
@@ -11,42 +10,29 @@ namespace ArtifactEnsemble.Artifacts
         public BazaarArtifact()
         {
             Init("Bazaar", "Adds a cleansing pool, scrapper, and a void chest to the space bazaar.", Properties.Resources.bazaar_on, Properties.Resources.bazaar_off);
-            On.RoR2.BazaarController.Awake += SpawnBazaarExtras;
+            On.RoR2.BazaarController.SetUpSeerStations += SpawnBazaarExtras;
         }
 
-        private void SpawnBazaarExtras(On.RoR2.BazaarController.orig_Awake orig, BazaarController self)
+        private void SpawnBazaarExtras(On.RoR2.BazaarController.orig_SetUpSeerStations orig, BazaarController self)
         {
             orig(self);
-            if (this.Enabled())
-            {
-                ArtifactEnsemble.Logger.LogInfo("Spawning bazaar extras.");
-                // Director stuff
-                DirectorPlacementRule directorPlacementRule = new DirectorPlacementRule();
-                directorPlacementRule.placementMode = DirectorPlacementRule.PlacementMode.Direct;
-                // Actual spawning
-                // Cleansing Pool
-                if (ArtifactEnsembleConfig.SpawnBazaarCleansingPool.Value)
-                {
-                    SpawnCard spawnCardCleansingPool = Resources.Load<SpawnCard>("SpawnCards/InteractableSpawnCard/iscShrineCleanse");
-                    GameObject spawnedInstanceCleansingPool = spawnCardCleansingPool.DoSpawn(new Vector3(-65.7f, -23.5f, -18.9f), Quaternion.identity, new DirectorSpawnRequest(spawnCardCleansingPool, directorPlacementRule, Run.instance.runRNG)).spawnedInstance;
-                    NetworkServer.Spawn(spawnedInstanceCleansingPool);
-                }
-                // Scrapper
-                if (ArtifactEnsembleConfig.SpawnBazaarScrapper.Value)
-                {
-                    SpawnCard spawnCardScrapper = Resources.Load<SpawnCard>("SpawnCards/InteractableSpawnCard/iscScrapper");
-                    GameObject spawnedInstanceScrapper = spawnCardScrapper.DoSpawn(new Vector3(-82.1f, -23.7f, -5.2f), Quaternion.identity, new DirectorSpawnRequest(spawnCardScrapper, directorPlacementRule, Run.instance.runRNG)).spawnedInstance;
-                    spawnedInstanceScrapper.transform.eulerAngles = new Vector3(0f, 72.6f, 0f);
-                    NetworkServer.Spawn(spawnedInstanceScrapper);
-                }
-                // Void Chest
-                if (ArtifactEnsembleConfig.SpawnBazaarVoidChest.Value)
-                {
-                    SpawnCard spawnCardVoidChest = Resources.Load<SpawnCard>("SpawnCards/InteractableSpawnCard/iscVoidChest");
-                    GameObject spawnedInstanceVoidCradle = spawnCardVoidChest.DoSpawn(new Vector3(-69.7f, -23.5f, -12.9f), Quaternion.identity, new DirectorSpawnRequest(spawnCardVoidChest, directorPlacementRule, Run.instance.runRNG)).spawnedInstance;
-                    NetworkServer.Spawn(spawnedInstanceVoidCradle);
-                }
-            }
+            
+            if (!this.Enabled()) return;
+            
+            ArtifactEnsemble.Logger.LogInfo("Spawning bazaar extras.");
+            
+            // Cleansing Pool
+            if(ArtifactEnsembleConfig.SpawnBazaarCleansingPool.Value)
+                TrySpawn("SpawnCards/InteractableSpawnCard/iscShrineCleanse", new Vector3(-65.7f, -23.5f, -18.9f), Vector3.zero);
+            
+            // Scrapper
+            if (ArtifactEnsembleConfig.SpawnBazaarScrapper.Value)
+                TrySpawn("SpawnCards/InteractableSpawnCard/iscScrapper", new Vector3(-82.1f, -23.7f, -5.2f),
+                    new Vector3(0f, 72.6f, 0f));
+            
+            // Void Chest
+            if (ArtifactEnsembleConfig.SpawnBazaarVoidChest.Value)
+                TrySpawn("SpawnCards/InteractableSpawnCard/iscVoidChest",new Vector3(-69.7f, -23.5f, -12.9f),Vector3.zero);
         }
     }
 }
